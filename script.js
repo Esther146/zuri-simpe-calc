93afdadc5b67f32c2e9ac3506d82ac67ae1a2c6b
Esther Anierobi <esther.anierobi@gmail.com>
2022-06-22 18:05:37 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// Building simple calculator without frontend
+
+let theOperator = prompt("Enter the operator: Example +, -, *, /, %, or ^");
+
+if (theOperator === '+'){
+    let num1 = parseFloat(prompt('Enter first value'));
+    let num2 = parseFloat(prompt('Enter second value'));
+    let result = num1 + num2;
+    alert(num1 + ' + ' + num2 + ' = ' + result);
+}
+else if (theOperator === '-'){
+    let num1 = parseFloat(prompt('Enter first value'));
+    let num2 = parseFloat(prompt('Enter second value'));
+    let result = num1 - num2;
+    alert(num1 + ' - ' + num2 + ' = ' + result);
+}
+else if (theOperator === '*'){
+    let num1 = parseInt(prompt('Enter first value'));
+    let num2 = parseInt(prompt('Enter second value'));
+    var result = num1 * num2;
+    prompt(num1 + ' * ' + num2 + ' = ' + result);
+}
+else if (theOperator === '/'){
+    let num1 = parseInt(prompt('Enter first value'));
+    let num2 = parseInt(prompt('Enter second value'));
+    var result = num1 / num2;
+    prompt(num1 + ' / ' + num2 + ' = ' + result);
+}
+else if (theOperator === '%'){
+    let num1 = parseInt(prompt('Enter first value'));
+    let num2 = parseInt(prompt('Enter second value'));
+    var result = num1 % num2;
+    prompt(num1 + ' % ' + num2 + ' = ' + result);
+}
+else if (theOperator === '^'){
+    let num1 = parseInt(prompt('Enter first value'));
+    let num2 = parseInt(prompt('Enter second value'));
+    var result = Math.pow(num1,num2);
+    prompt(num1 + ' ^ ' + num2 + ' = ' + result);
+}
+
+else{
+    alert('Please enter a valid mathematical operator');
+}
+
+
